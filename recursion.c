ccff35eae063ab98c062c40645b1f0485ea1b03c
Gaurav <gauravkr7265@gmail.com>
2023-02-12 22:04:37 +0530

    added hw set

--- recursion.c
+++ recursion.c
@@ -3,10 +3,20 @@
             // RECURSION - When a function calls itself, it's called recursion.
             // EXAMPLE - Print "Hello World" 5 times.
 
+void printHW(int count);
+
 int main(){
-
-
-
+    printHW(5);
 
     return 0;
+}
+
+    // recursive function
+
+void printHW(int count){
+    if(count == 0){
+        return;
+    }
+    printf("Hello World\n");
+    printHW(count-1);
 }
