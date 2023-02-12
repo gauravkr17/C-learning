97c1ea9062da764932606bd4e5de2f2010fa424d
Gaurav <gauravkr7265@gmail.com>
2023-02-12 10:36:42 +0530

    added question on function

---
+++ function_question03.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+// Print Table of number entered by the user using function.
+
+void printTable();
+
+int main(){
+    int n;
+    printf("Enter number : ");
+    scanf("%d" , &n);
+
+    printTable(n);   //argument / actual parameter
+
+    return 0;
+}
+
+void printTable(int n){    //parameter / formal parameter
+    for(int i=1 ; i<=10 ; i++){
+        printf("%d\n" , i*n);
+    }
+
+}
